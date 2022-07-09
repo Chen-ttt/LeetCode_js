8923dd777854e045f2736be13da2acb1449cbdce
Tong Chen <1141425427@qq.com>
2022-07-09 22:48:58 +0100

    LinkedList Ex.206 Reverse Singly-linked List II

--- LinkedList/206_reverseList.js
+++ LinkedList/206_reverseList.js
@@ -25,3 +25,24 @@
     head.next = null; // 断开当前节点和下一节点的关系
     return newHead; // 永远传递的都是最后一个节点, 即新的头节点
 };
+
+/**
+ * 迭代
+ *   使用三个指针完成: prev cur next分别指向上一个节点,当前处理的节点和下一个节点
+ * 
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(1)
+ */
+reverseList = function(head) {
+    if(head === null) return head; // Special case: []
+    var prevNode = null,
+        curNode = head,
+        nextNode = null;
+    while(curNode){
+        nextNode = curNode.next; // 用next指向下一个节点, 不然当前节点反转后, 下一个节点将丢失
+        curNode.next = prevNode; // 局部反转
+        prevNode = curNode; // 将prev指针向前挪动
+        curNode = nextNode; // 将cur指针向前挪动, 挪动后若指向null, 说明反转完成, 将不会进入下一轮循环
+    }
+    return prevNode; // 结束时prev即为新的头节点
+};
