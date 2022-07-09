ec00f319b5ace5e74eb338674479647cf0241395
Tong Chen <1141425427@qq.com>
2022-07-09 23:09:51 +0100

    LinkedList Ex.876 Middle with Slow & Fast  Pointer

--- LinkedList/876_middleNode.js
+++ LinkedList/876_middleNode.js
@@ -10,15 +10,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  * 
- * 时间复杂度 O(n) n为结点数, 需要遍历一遍列表
+ * 时间复杂度 O(n) n为结点数, 只需要遍历一遍列表
  * 空间复杂度 O(1) 只要常数空间存放快慢指针
  */
 
 /**
  * 快慢指针
  * 用两个指针slow fast一起遍历链表, slow一次走一步, fast一次走两步
- *   当有偶数个节点, fast指向null, slow在中间
- *   当有奇数个节点, fast指向最后一个元素, slow在中间两个元素的后面那个
+ *   1. 当有偶数个节点, fast指向null, slow在中间
+ *   2. 当有奇数个节点, fast指向最后一个元素, slow在中间两个元素的后面那个
  * 因此 当fast指向null或者最后一个元素时, 结束循环
  */
  var middleNode = function(head) {
