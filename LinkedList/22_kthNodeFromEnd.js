cc69417f3f928eb449dd09e362cf1bedcd66aa77
Tong Chen <1141425427@qq.com>
2022-07-21 18:18:32 +0100

    LinkedList Ex.22 Kth Node from End in LinkedList

--- LinkedList/22_kthNodeFromEnd.js
+++ LinkedList/22_kthNodeFromEnd.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-07-21 18:03:07
- * @LastEditTime: 2022-07-21 18:06:02
+ * @LastEditTime: 2022-07-21 18:17:46
  * @LastEditors:  
  */
 /**
@@ -16,11 +16,13 @@
  * 空间复杂度 O(1)
  */
 var getKthFromEnd = function (head, k) {
+  if (head == null || k == 0) return null // 防止输入 [] 3 或 [1,2,3] 0
   var fast = head,
     slow = head
 
   for (let i = 0; i < k - 1; i++) {
     fast = fast.next
+    if (fast == null) return null // 防止输入 [1,2,3,4] 8
   }
 
   while (fast.next != null) {
