27010d9839587a4f5f915b8d13ccf4067f3c6311
Tong Chen <1141425427@qq.com>
2022-08-04 16:42:31 +0100

    DFS Ex.17 Letter Combination in Phone Number

--- DFS + Backtracking/17_letterCombination.js
+++ DFS + Backtracking/17_letterCombination.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-07-29 14:18:06
- * @LastEditTime: 2022-07-29 14:32:33
+ * @LastEditTime: 2022-08-04 16:42:05
  * @LastEditors:  
  */
 /**
@@ -37,13 +37,15 @@ var letterCombinations = function (digits) {
     // 1. 递归出口 - 已走到digits末尾, 将这条路的结果放入result并结束当前递归, 结束后即可回溯到上一节点
     if (i >= digits.length) {
       result.push(path)
-    } else { // 2. 没有走到末尾, 需要继续递归
-      // 3. 由于每个数字对应的字母个数不同, 因此只能循环进行递归, 每次取一种可能的字母
-      for (let ch of list[digits[i] - 2]) { // 注意. 这里有隐式类型转换
-        dfs(i + 1, path + ch)
-        // 注意. 不可以用++i, 因为会改变当前作用域中i的值, 在下一次回溯到这里的时候, i会递增
-        // 而在每次for循环的递归中, 其实i都应该是不变的, 因此使用i+1
-      }
+      return
+    }
+
+    // 2. 没有走到末尾, 需要继续递归
+    // 3. 由于每个数字对应的字母个数不同, 因此只能循环进行递归, 每次取一种可能的字母
+    for (let ch of list[digits[i] - 2]) { // 注意. 这里有隐式类型转换
+      dfs(i + 1, path + ch)
+      // 注意. 不可以用++i, 因为会改变当前作用域中i的值, 在下一次回溯到这里的时候, i会递增
+      // 而在每次for循环的递归中, 其实i都应该是不变的, 因此使用i+1
     }
   }
 
