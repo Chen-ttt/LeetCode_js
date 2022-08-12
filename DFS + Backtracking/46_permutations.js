b0ecd05f02f757765017f238dd886a0eb8054260
Tong Chen <1141425427@qq.com>
2022-08-12 02:05:17 +0100

    DFS Ex.46 Permutations I with DFS & Backtracking

--- DFS + Backtracking/46_permutations.js
+++ DFS + Backtracking/46_permutations.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-04 13:36:00
- * @LastEditTime: 2022-08-04 13:36:12
+ * @LastEditTime: 2022-08-12 02:04:39
  * @LastEditors:  
  */
 /**
@@ -10,31 +10,35 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// 排列问题 - 需要考虑顺序, 因此[1, 2]和[2, 1]是两个解
+// 因此for循环需要每次都从0开始
+// 但这会引起一个问题, 每次从0开始, 必定会重复的取到同一个元素, 因此需要used数组标记当前path已使用的元素, 以跳过这些元素
+
 var permute = function (nums) {
-  let result = []
+  let result = [],
+    path = [],
+    used = new Array(nums.length).fill(false)
 
-  const length = nums.length
-  let flag = new Array(length).fill(false)
-
-  var dfs = (path) => {
-    if (path.length === length) {
+  let dfs = () => {
+    if (path.length === nums.length) {
       result.push([...path])
       return
     }
 
-    for (let i = 0; i < length; i++) {
-      if (flag[i]) continue
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i]) continue // 如果该数组被标记为true, 表示当前path已使用, 跳过
 
       path.push(nums[i])
-      flag[i] = true
+      used[i] = true
 
-      dfs(path)
+      dfs(i)
 
       path.pop()
-      flag[i] = false
+      used[i] = false // 回溯时, 元素弹出, 标记复原为false
     }
   }
 
-  dfs([])
+  dfs()
   return result
 }
